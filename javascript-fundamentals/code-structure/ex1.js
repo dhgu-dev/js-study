1cc9ba0c056c5d3874bfd0428f057e900603455b
dhgu <dhgu.dev@gmail.com>
2022-09-05 13:30:39 +0900

    ch2-2 Done

---
+++ javascript-fundamentals/code-structure/ex1.js
@@ -0,0 +1,26 @@
+// hello world를 두 개의 alert statement로 나눈 예시
+alert('hello');
+alert('world');
+
+// general
+alert('hello');
+alert('world');
+
+/* automatic semicolon insertion
+alert('hello')
+alert('world')
+*/
+
+/* 세미콜론을 자동으로 삽입해주지 못하는 상황
+alert('에러가 발생') 
+[1, 2].forEach(alert);
+*/
+
+alert('제대로 동작');
+[1, 2].forEach(alert);
+
+// 한 줄 주석 ctrl + /
+
+/* 
+  여러 줄 주석 alt + shift + a
+ */
